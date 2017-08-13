31ada23746d58ac6473d3dfcb5a9b53917e198a7
Konarkcher <konarkcher@yandex.ru>
2017-08-13 22:15:01 +0300

    db path cache and some signature fixes

--- model/product.py
+++ model/product.py
@@ -1,8 +1,8 @@
 class Product:
 
     def __init__(self, from_db):
-        self.id = from_db[0]
+        self.product_id = from_db[0]
         self.name = from_db[1]
         self.count = from_db[2]
         self.price = from_db[3]
-        self.reserve = from_db[4]
+        self.reserved = from_db[4]
