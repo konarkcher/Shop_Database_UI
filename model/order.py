ad1272152d26e629b2215e900a66dc5d04172310
alloky <alloky2011@gmail.com>
2017-08-14 16:31:45 +0300

    order fixed

--- model/order.py
+++ model/order.py
@@ -9,19 +9,20 @@ class Order:
             if p.id in self._cart:
                 self._cart[p.id].count += 1
             else:
+                p.count = 1
                 self._cart[p.id] = p
-        self._sum = sum([x.price for x in self._cart.values()])
+        self._sum = sum([x.price*x.count for x in self._cart.values()])
 
     def set_customer(self, customer):
         self._customer = customer
 
     def get_cart(self):
-        return self._cart.values()
+        return list(self._cart.values())
 
     def remove_from_cart(self, products):
         st = set([x.id for x in products])
         self._cart = dict([(x.id,x) for x in self._cart.values() if x.id not in st])
-        self._sum = sum([x.price for x in self._cart.values()])
+        self._sum = sum([x.price*x.count for x in self._cart.values()])
 
     def get_initials(self):
         return self._customer.get_initials()
